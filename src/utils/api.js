3bdfb121d14a844cd8ea487637ce1ca281004115
jwon114 <jthgwong@gmail.com>
2020-05-31 14:46:34 +0100

    open weather map https

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,9 @@ const API_KEY = '6c8ca7a74ad9d3301728396fc82b6227';
 
 export default { 
   fetchCurrentWeather: () => (
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=london,uk&APPID=${API_KEY}&units=metric`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=london,uk&APPID=${API_KEY}&units=metric`)
   ),
   fetchFiveDayForecast: () => (
-    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=${API_KEY}&units=metric`)
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=london,uk&APPID=${API_KEY}&units=metric`)
   )
 }
